7ef25cb3aa05e1e028831c1f5cc39080b475c945
peroo4god <peroo4god@gmail.com>
2023-09-09 01:41:24 -0700

    puts commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is main funtion
+ *
+ * Return: is always 0
+ */
+
+int main(void)
+{
+	puts("⧵"Programming is like building a multilingual puzzle);
+return (0);
+
+}
