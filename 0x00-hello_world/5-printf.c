41e44f9616dfc079a361886399c44d0fb8ff314c
peroo4god <peroo4god@gmail.com>
2023-09-09 05:02:23 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - this is the main function read by the compiler.
+ * main - this is the main function read by the compiler
  *
- * Return: return 0 if the program run successfully
+ * Return: 0 if the program run successfully
  */
 
-int main(void);
+int main(void)
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
