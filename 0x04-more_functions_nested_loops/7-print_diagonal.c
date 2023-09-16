bb574e8403f26b2af32bd389e7fd059bc4741da7
peroo4god <peroo4god@gmail.com>
2023-09-15 20:28:40 -0700

    7-print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,7 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	for (i =0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
@@ -20,6 +20,5 @@ void print_diagonal(int n)
 		}
 		_putchar('\\');
 		_putchar('\n');
-		
 	}
 }
