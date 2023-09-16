604961fec55fd20aca7f0fdec1fdf6aec2f301c9
peroo4god <peroo4god@gmail.com>
2023-09-15 19:35:03 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers -  function that prints numbers from 0 to 14, 10 times
+ *
+ * Return: 0-14 success followed by a new line
+ */
+
+void more_numbers(void)
+{
+
+int x, y;
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			_putchar('x');
+			_putchar(y % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
